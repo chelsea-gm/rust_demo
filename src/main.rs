eb36bf06818eab170ede871d29f09c4beb01f080
chelsea-gm <97588283+chelsea-gm@users.noreply.github.com>
2022-01-12 03:10:07 -0500

    Added game state structure
    Added a basic data structure for holding game state info.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,33 @@ extern crate raylib;
 
 use raylib::prelude::*;
 
+struct Game_State {
+    player_x: i32;
+    player_y: i32;
+    
+    sword_state: i32;
+    
+    background_color: Color;
+}
+
+fn get_background_color(game_state: &Game_State) -> Color
+{
+    return game_state.background_color;
+}
+
 fn main() {
+    let screen_width = 800;
+    let screen_height = 600;
+    let target_fps = 60;
+
     let (mut rl, thread) = raylib::init()
-        .size(800, 600)
+        .size(screen_width, screen_height)
         .title("Rust Demo - Standalone app showcasing a Raylib binding for Rust - Chelsea")
         .build();
+
+    rl.set_target_fps(target_fps)
+    
+    let game_state = GameState { screen_width / 2, screen_height / 2, 0, Color::GREEN };
 
     while !rl.window_should_close() &&
         !rl.is_key_pressed(raylib::consts::KeyboardKey::KEY_W) &&
@@ -16,7 +38,7 @@ fn main() {
     {
         let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::RAYWHITE);
+        d.clear_background(get_background_color(game_state));
 
         d.draw_text("Hello, world!", 10, 10, 20, Color::BLACK);
         d.draw_text("Press W, A, S, or D to continue...", 10, 30, 20, Color::BLACK);
@@ -26,7 +48,7 @@ fn main() {
     {
         let mut d = rl.begin_drawing(&thread);
         
-        d.clear_background(Color::RAYWHITE);
+        d.clear_background(get_background_color(game_state));
         
         d.draw_text("Thank you for playing! Press ESC to exit...", 10, 10, 20, Color::BLACK);
     }
